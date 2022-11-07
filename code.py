89d0b0762c7c1f128b6829c080ab19cd7b423b07
Souvik Ghosh <114401034+Souvik0216J@users.noreply.github.com>
2022-11-07 21:56:30 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,15 @@
+import turtle, time
+
+colors = ['red', 'purple', 'blue', 
+        'green', 'orange', 'yellow']
+        
+t = turtle.Pen()
+turtle.bgcolor('black')
+
+for i in range(360):
+	t.pencolor(colors[i%6])
+	t.width(i//100 + 1)
+	t.forward(i)
+	t.left(59)
+
+time.sleep(15)
